a7e74d867cb4f7b21b74c06909bb9633229ecafc
Muhammed Nazim <77053815+the-nazim@users.noreply.github.com>
2025-01-18 12:09:14 +0530

    Update navigation

--- navmaini_testbed.py
+++ navmaini_testbed.py
@@ -1,4 +1,4 @@
-#::::::::::::::::::::::::::::::::::::  LIDAR NAVIGATION CODE FOR MAINI 2 ::::::::::::::::::::::::::::::::::::#
+#::::::::::::::::::::::::::::::::::::  LIDAR NAVIGATION CODE FOR MAINI 2  ON TEST ::::::::::::::::::::::::::::::::::::#
 #!/usr/bin/env python3
 import math 
 import numpy as np   
